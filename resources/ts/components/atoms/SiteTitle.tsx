577d80ae83db5b9361fcb952bd2fa6cfdc3bb38e
hitomiOYAMA <it.is.a.fishstory@gmail.com>
2023-06-20 12:38:46 +0900

    Oyamaの作成

---
+++ resources/ts/components/atoms/SiteTitle.tsx
@@ -0,0 +1,94 @@
+import React, { useRef, MouseEvent } from "react";
+import styled from 'styled-components';
+
+export const SiteTitle: React.FC = () => {
+
+    
+
+    const func = (e: MouseEvent) => {
+        // マウスの座標を取得して 中心からの距離取得、 そこからpararaxValで距離を調整
+        let x = (xCenter - e.pageX) / parallaxVal;
+        let y = (yCenter - e.pageY) / parallaxVal;
+
+        if(!targetRef.current) {
+            return
+        }
+
+        if(x > 30 || x < -30) {
+            targetRef.current.style.transform = `translate(0px, 0px)`;
+            return
+        }
+
+        if(y > 30 || y < -30) {
+            targetRef.current.style.transform = `translate(0px, 0px)`;
+            return
+        }
+
+        // パララックスさせる要素にstyleの指定
+        targetRef.current.style.transform = `translate(${-x}px, ${-y}px)`;
+
+    }
+    
+    //パララックスを行うエリア取得
+    const activeAreaRef = useRef<HTMLDivElement>(null);
+    // パララックスのターゲット取得
+    const targetRef = useRef<HTMLDivElement>(null);
+    // ウィンドウの中心の取得
+    const xCenter = window.innerWidth / 2;
+    const yCenter = window.innerHeight / 2;
+    // パララックスで移動させる距離
+    const parallaxVal = 10; //中心からマウスの距離の10分１移動
+    
+    return (
+        <StyledPararaxArea ref={activeAreaRef} onMouseMove={func}>
+            <StyledJsPararax id="js-parallax-item" ref={targetRef}></StyledJsPararax>
+            <StyledO />
+            <StyledText>yama</StyledText>
+        </StyledPararaxArea>
+    )
+}
+
+interface Props {
+    ref?: React.RefObject<HTMLDivElement>
+}
+
+const StyledPararaxArea = styled.div<Props>`
+    position: relative;
+    background-color: #f8f8f8;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+`
+
+const StyledJsPararax = styled.div`
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    border-radius: 20px;
+    background-color: black;
+    transition: transform .3s linear .001s;
+    bottom: 150px;
+    right: 270px;
+`
+
+const StyledO = styled.div`
+    position: absolute;
+    bottom: 100px;
+    right: 220px;
+    z-index: 100;
+    width: 100px;
+    height: 100px;
+    border-radius: 100px;
+    border: 10px solid black;
+`
+
+const StyledText = styled.p`
+    font-size: 50px;
+    position: absolute;
+    right: 70px;
+    bottom: 36px;
+    font-weight: 900;
+`
--- resources/ts/index.tsx
+++ resources/ts/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { SiteTitle } from './components/atoms/SiteTitle';
 
 const container = document.getElementById('app')!;
 const root = createRoot(container);
 
 root.render(
     <React.StrictMode>
-        <div>Hello world</div>
+        <div>HelloWorld</div>
+        <SiteTitle />
     </React.StrictMode>
 )
